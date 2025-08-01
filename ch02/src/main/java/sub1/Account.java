a76b3cb297907e470879921bae87a5bead0dac6a
sub6550 <sub6550@naver.com>
2025-08-01 16:49:02 +0900

    2장. JSP 기본

--- ch02/src/main/java/sub1/Account.java
+++ ch02/src/main/java/sub1/Account.java
@@ -1,5 +1,39 @@
 package sub1;
 
-public class Account {
+import java.io.IOException;
 
+import jakarta.servlet.jsp.JspWriter;
+
+public class Account {
+	
+	private String bank;
+	private String id;
+	private String name;
+	private int balance;
+	
+	public Account(String bank, String id, String name, int balance) {
+		this.bank = bank;
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+	// deposit : 입금 함수
+	public void deposit(int money) {
+		this.balance += money;
+	}
+	// withdraw : 출금 함수
+	public void withdraw(int money) {
+		this.balance -= money;
+	}
+
+	// JspWriter : JSP에서 웹 페이지에 HTML 출력할 때 쓰는 내장 객체
+	public void show(JspWriter writer) throws IOException{ // throws IOException 예외처리
+		writer.println("<p>");
+		writer.println("은행명 : " + this.bank + "<br/>");
+		writer.println("계좌번호 : " + this.id + "<br/>");
+		writer.println("입금주 : " + this.name + "<br/>");
+		writer.println("현재잔액 : " + this.balance + "<br/>");
+		writer.println("</p>");
+		// writer.println : 입출력 중 오류가 발생할 수 있는 코드라서 예외처리를 해줌
+	}
 }
--- ch02/src/main/webapp/inc/_footer.jsp
+++ ch02/src/main/webapp/inc/_footer.jsp
@@ -1,12 +1,31 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%--
+	inc 폴더는 include 의 줄임말로, "반복적으로 포함시킬 공통 파일들을 모아두는 폴더" 이다.
+	주로 header, footer, sidebar, navigation, menu 등과 같이 웹 페이지에서
+	공통적으로 사용되는 부분이 이에 해당 된다.
+	
+	이렇게 정리를 하면, 프로젝트 전체의 일관성 유지와 유지보수가 수월해진다.
+	
+	그러나 inc 폴더에 jsp 파일들은 단순히 공통 컴포넌트를 정리한 파일일뿐,
+	직접 페이지별로 include 를 시켜주어야 해당 페이지에 적용된다.
+	
+	-------------------------------------------
+	ex: <%@ include file="/inc/_header.jsp" %>
 
-</body>
-</html>
+		<h2>유저 목록 페이지입니다</h2>
+
+		<%@ include file="/inc/_footer.jsp" %>
+	-------------------------------------------
+	
+	[정적 include]
+	<%@ include file="..." %>
+	-> 컴파일 시점에 내용을 복사해 넣는 느낌.
+
+	[동적 include]
+	<jsp:include page="..." />
+	-> 실행 시점에 포함.
+ --%>
+
+<footer>
+	<h3>Footer 파일입니다.</h3>
+</footer>
