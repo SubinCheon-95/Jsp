32fdb5660fd19a756ca468c834016be842495382
sub6550 <sub6550@gmail.com>
2025-08-07 23:21:07 +0900

    고객 정보를 조회/등록/수정/삭제 구현

--- ch05/src/main/java/vo/User5VO.java
+++ ch05/src/main/java/vo/User5VO.java
@@ -1,5 +1,39 @@
 package vo;
 
 public class User5VO {
-
+	
+	private String name;
+	private boolean gender;
+	private int age;
+	private String addr;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	@Override
+	public String toString() {
+		return "User5VO [name=" + name + ", gender=" + gender + ", age=" + age + ", addr=" + addr + "]";
+	}
 }
